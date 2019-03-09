6c1f3961dac5cd49e347b88aad360d35f21f8d08
arifdroid <arifdroiddeveloper@gmail.com>
2019-03-09 12:39:20 +0800

    working registration, work on log in, activity flow

--- app/src/main/java/com/example/a6march_firestorefinalize/employee_experience/RegistrationActivity.java
+++ app/src/main/java/com/example/a6march_firestorefinalize/employee_experience/RegistrationActivity.java
@@ -72,7 +72,7 @@ public class RegistrationActivity extends AppCompatActivity {
 
    // private boolean docExist;   //this is always true, assume if user registered, doc exist, just wait to prove, if otherwise add new doc
 
-
+    private String nameRegistered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -196,6 +196,24 @@ public class RegistrationActivity extends AppCompatActivity {
 
                 if(task.isSuccessful()){
 
+                    //need to check of admin document exist first.
+
+
+
+                    CollectionReference collectionReference_checkaadmin_exist = FirebaseFirestore.getInstance().collection("adminsOffices");
+//
+                    Query query_checkadmin = collectionReference_checkaadmin_exist.whereEqualTo("phone_admin",company_phone_user_enter);
+//
+                    query_checkadmin.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                        @Override
+                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+
+                            //means if size > 1 , document exist.
+                            if (queryDocumentSnapshots.size()>=1){
+                                    Log.i("check ", "admin number recognized");
+
+
                     Log.i("checkk "," task is successful");
 
                     //test if user is registered by admin
@@ -214,15 +232,19 @@ public class RegistrationActivity extends AppCompatActivity {
                             int i =0;
                             int j =0;
 
-                            ArrayList<String> checkUser = new ArrayList<>();
+                          //  ArrayList<String> checkUser = new ArrayList<>();
 
                             int size = queryDocumentSnapshots.size();
 
                             Log.i("checkk ", "stop 1");
 
+                            Log.i("checkk flow ","stop 00");
+
                             for(QueryDocumentSnapshot documentSnapshot:queryDocumentSnapshots){ //this cycle extract document with query
 
                                 Log.i("checkk ", "stop 2");
+
+                                Log.i("checkk flow ","stop 11");
 
                                 map= documentSnapshot.getData(); //every single document consist of hashmap
                                  j++;
@@ -253,12 +275,19 @@ public class RegistrationActivity extends AppCompatActivity {
 
                                 }
 
-                           userLoggedIn();  //we just log in, if user is registered
+                                nameRegistered = editText_employee_name.getText().toString();
+
+
+                           checkIfAlreadyRegistered();  // user is registered by admin, by has user already registered before and got existing
+                                //document
 
                             }
 
+
+                            Log.i("checkk flow ","stop 22");
                             if(size<1){
 
+                                Log.i("checkk flow ","stop 77");
                                 Log.i("checkk", "no document present?");
 
                                 logOutFromFirebaseAuth();
@@ -286,7 +315,15 @@ public class RegistrationActivity extends AppCompatActivity {
                         }
                     });
 
+                }else {
 
+                    Log.i("check ", "admin number not recognized");
+
+
+                }
+
+            }
+        });
 
 
                 } else {
@@ -305,6 +342,7 @@ public class RegistrationActivity extends AppCompatActivity {
     }
 
     private void logOutFromFirebaseAuth() {
+        Log.i("checkk flow ","stop 88");
 
         FirebaseAuth.getInstance().signOut();
         Log.i("checkk","logged OUT");
@@ -312,9 +350,110 @@ public class RegistrationActivity extends AppCompatActivity {
 
     private void userLoggedIn() {
 
+
+        Log.i("checkk flow ","stop 99");
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    //create document with reference of phone number
+    // we need to check whether admin exist or not, else dont proceed
+
+
+    //if exist, probably already registered. dont log, in, ask to retrieve password.
+
+
+
+
+    /// if document not exist, below
+
+    DocumentReference documentReference = FirebaseFirestore.getInstance().collection("employees_to_offices").document(company_phone_user_enter+"_company_uid")
+            .collection("uid_employee_this").document(user.getPhoneNumber()+"final");
+
+    //unique uid for scorecard, uid from user/employee document
+    String score_card_uid = user.getPhoneNumber()+"score_card_uid";
+
+
+    //create document for score card with reference from user uid.
+
+    DocumentReference documentReference_score_uid = FirebaseFirestore.getInstance()
+            .collection("employees_to_offices").document(company_phone_user_enter+"_company_uid")
+                    .collection("score_ref_collection_this").document(score_card_uid);
+
+    Map<String,Object> ll = new HashMap<>();
+
+    ll.put("name",nameRegistered);
+    ll.put("phone",user.getPhoneNumber());
+    ll.put("my_score_ref",score_card_uid);
+
+    documentReference.set(ll);
+
+    Map<String,Object> hh = new HashMap<>(); //now how do we populate this, we populate here or later?
+
+    hh.put("my_score","0");
+
+    documentReference_score_uid.set(hh);
+
+    Log.i("checkk uid"," score card "+score_card_uid );
+
     textViewMessageRegistration.setText("you are logged in");
     Log.i("checkk","logged IN");
 
+
+    }
+
+    private void checkIfAlreadyRegistered() {
+
+        Log.i("checkk flow ","stop 33");
+
+        CollectionReference collectionReference_check_ifResisteredAlready =
+                FirebaseFirestore.getInstance().collection("employees_to_offices").document(company_phone_user_enter+"_company_uid")
+                        .collection("uid_employee_this");
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        Query queryhere = collectionReference_check_ifResisteredAlready.whereEqualTo("phone",user.getPhoneNumber());
+
+        queryhere.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+                int size = queryDocumentSnapshots.size();
+
+
+                int i =0;
+
+                Log.i("checkk flow 555","i : "+i);
+
+                for(QueryDocumentSnapshot documentSnapshot: queryDocumentSnapshots){
+                    i++;
+
+                    Log.i("checkk  i flow ","i : "+i);
+
+                    String s = documentSnapshot.getData().get("phone").toString();
+
+                    Log.i("checkk flow document ","data is: "+s);
+
+                }
+
+                Log.i("checkk flow 666","i : "+i);
+
+                if(size>=1){ //means user document already exist
+
+                    Log.i("checkk flow ","stop 55");
+                    logOutFromFirebaseAuth_Already_registered();
+
+                }else {
+                    Log.i("checkk flow ","stop 66");
+                    userLoggedIn();
+                }
+            }
+        });
+
+    }
+
+    private void logOutFromFirebaseAuth_Already_registered() {
+
+        FirebaseAuth.getInstance().signOut();
+        Log.i("checkk","already registered, please contact admin");
 
     }
 
